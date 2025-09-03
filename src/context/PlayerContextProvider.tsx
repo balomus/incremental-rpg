3b7d3c15cb83aff593cbd863632b459b765624fd
Stephen <balom.us@gmail.com>
2025-09-03 15:29:12 -0400

    Small refactor to playercontext

--- src/context/PlayerContextProvider.tsx
+++ src/context/PlayerContextProvider.tsx
@@ -36,34 +36,42 @@ export const PlayerContext = createContext<PlayerContextType>({
 const PlayerContextProvider = ({ children }: any) => {
   const [player, setPlayer] = useState<Player>(initialPlayerInfo);
 
+  const killPlayer = () => {
+    setPlayer({
+      ...player,
+      currentHealth: player.maxHealth,
+      currentMana: player.maxMana,
+      experience: 0,
+      gold: Math.round(player.gold * 0.9),
+    });
+
+    countDown(10, deathTitle, deathStr);
+  };
+
+  const levelUpPlayer = () => {
+    const remainder = player.experience - player.maxExperience;
+    setPlayer({
+      ...player,
+      level: player.level + 1,
+      maxExperience: player.maxExperience * 2,
+      currentHealth: player.maxHealth + 5,
+      maxHealth: player.maxHealth + 5,
+      currentMana: player.maxMana + 5,
+      maxMana: player.maxMana + 5,
+      strength: player.strength + 1,
+      experience: remainder,
+    });
+
+    countDown(5, levelUpTitle, levelUpStr);
+  };
+
   useEffect(() => {
     if (player.currentHealth <= 0) {
-      setPlayer({
-        ...player,
-        currentHealth: player.maxHealth,
-        currentMana: player.maxMana,
-        experience: 0,
-        gold: Math.round(player.gold * 0.9),
-      });
-
-      countDown(10, deathTitle, deathStr);
+      killPlayer();
     }
 
     if (player.experience >= player.maxExperience) {
-      const remainder = player.experience - player.maxExperience;
-      setPlayer({
-        ...player,
-        level: player.level + 1,
-        maxExperience: player.maxExperience * 2,
-        currentHealth: player.maxHealth + 5,
-        maxHealth: player.maxHealth + 5,
-        currentMana: player.maxMana + 5,
-        maxMana: player.maxMana + 5,
-        strength: player.strength + 1,
-        experience: remainder,
-      });
-
-      countDown(5, levelUpTitle, levelUpStr);
+      levelUpPlayer();
     }
   }, [player]);
 
